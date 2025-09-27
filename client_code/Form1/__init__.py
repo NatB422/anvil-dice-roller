a3f2135c18eb66f220aad0d69f057bd19026a0bd
natb422@gmail.com <natb422@gmail.com>
2025-09-27 13:57:37 +0000

    Edited theme.css, standard-page.html and Form1; Deleted themes/.gitkeep, themes/rust/.gitkeep, themes/rust/diffuse-dark.png, themes/rust/diffuse-light.png and 4 more

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -2,34 +2,41 @@ from ._anvil_designer import Form1Template
 from anvil import *
 import anvil.js
 
+
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-
+    self.dice = None
+    
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
+    if not self.dice:
+      dice_box_module = anvil.js.import_from("https://unpkg.com/@3d-dice/dice-box@1.1.3/dist/dice-box.es.min.js")
+      dice_box = dice_box_module.default
+      
+      dice_options = {
+        "assetPath": "assets/",
+        "origin": "https://unpkg.com/@3d-dice/dice-box@1.1.3/dist/",
+        "container": ".xypanel-0",
+        "theme": "diceOfRolling",
+        "themeColor": "#feea03",
+        "externalThemes": {
+          "diceOfRolling": "https://www.unpkg.com/@3d-dice/theme-dice-of-rolling@0.2.1",
+        },
+        "startingHeight": 8,
+        "throwForce": 6,
+        "spinForce": 5,
+        "lightIntensity": 0.9,
+        "scale": 8,
+        "offscreen": True,
+      }
+      self.dice = dice_box(dice_options)
+  
+      anvil.js.await_promise(self.dice.init())
     
-    if self.dice:
-      self.dice.clear()
-      self.dice.roll("1d6", {"theme": "rust"})
-      self.dice.show()
-
-  def DiceBox_show(self, **event_args):
-    """This method is called when the X-Y panel is shown on the screen"""
-    dice_box_module = anvil.js.import_from("https://cdn.jsdelivr.net/npm/@3d-dice/dice-box@1.1.4/dist/dice-box.es.min.js")
-    dice_box = dice_box_module.default
-
-    asset_folder = f"{anvil.server.get_app_origin()}/_/theme/themes/"
-    print(asset_folder)
-    self.dice = dice_box({
-      "target": '#DiceBox',
-      "themes": ["rust"],
-      "assetPath": asset_folder,
-    })
-    self.dice.init()
-    # dice.roll("1d6")
-    
-    
+    self.dice.clear()
+    self.dice.roll("1d6")
+    self.dice.show()

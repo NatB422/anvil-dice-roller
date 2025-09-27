6853c2765128e79b742f0dd270f8e71cedef54bd
natb422@gmail.com <natb422@gmail.com>
2025-09-27 12:21:57 +0000

    Created themes/.gitkeep, themes/rust/.gitkeep, themes/rust/diffuse-dark.png, themes/rust/diffuse-light.png and 4 more; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,6 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
-
+import anvil.js
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -8,3 +8,28 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    
+    if self.dice:
+      self.dice.clear()
+      self.dice.roll("1d6", {"theme": "rust"})
+      self.dice.show()
+
+  def DiceBox_show(self, **event_args):
+    """This method is called when the X-Y panel is shown on the screen"""
+    dice_box_module = anvil.js.import_from("https://cdn.jsdelivr.net/npm/@3d-dice/dice-box@1.1.4/dist/dice-box.es.min.js")
+    dice_box = dice_box_module.default
+
+    asset_folder = f"{anvil.server.get_app_origin()}/_/theme/themes/"
+    print(asset_folder)
+    self.dice = dice_box({
+      "target": '#DiceBox',
+      "themes": ["rust"],
+      "assetPath": asset_folder,
+    })
+    self.dice.init()
+    # dice.roll("1d6")
+    
+    

b0546c7e3a5d0e350b2de9492bbb6bb01d44ec80
natb422@gmail.com <natb422@gmail.com>
2025-09-29 20:27:35 +0000

    Edited theme.css and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -10,19 +10,22 @@ class Form1(Form1Template):
 
     # Any code you write here will run before the form opens.
     self.dice = None
+    self.dice_box_module = anvil.js.import_from("https://unpkg.com/@3d-dice/dice-box@1.1.3/dist/dice-box.es.min.js").default
     
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
     if not self.dice:
-      dice_box_module = anvil.js.import_from("https://unpkg.com/@3d-dice/dice-box@1.1.3/dist/dice-box.es.min.js")
-      dice_box = dice_box_module.default
+      self.DiceBox.add_component(anvil.XYPanel(
+        width=400, height=400,
+        margin=40,
+      ))
       
       dice_options = {
         "assetPath": "assets/",
         "origin": "https://unpkg.com/@3d-dice/dice-box@1.1.3/dist/",
-        "container": ".xypanel-0",
+        "container": ".xypanel-1",
         "theme": "diceOfRolling",
-        "themeColor": "#feea03",
+        "themeColor": "#008080",
         "externalThemes": {
           "diceOfRolling": "https://www.unpkg.com/@3d-dice/theme-dice-of-rolling@0.2.1",
         },
@@ -30,15 +33,11 @@ class Form1(Form1Template):
         "throwForce": 6,
         "spinForce": 5,
         "lightIntensity": 0.9,
-        "scale": 8,
-        "offscreen": True,
+        "scale": 6,
+        "offscreen": False,
         "onRollComplete": self.update_roll_result,
       }
-      self.dice = dice_box(dice_options)
-      image_asset = f"{anvil.server.get_app_origin()}/_/theme/DiceTray.png"
-  
-      # Set the background property of the XYPanel
-      # self.DiceBox.background = image_asset
+      self.dice = self.dice_box_module(dice_options)
   
       anvil.js.await_promise(self.dice.init())
     
@@ -57,7 +56,15 @@ class Form1(Form1Template):
   def update_roll_result(self, results):
     #dice_outcomes = results[0]["rolls"]
     #total = sum(d["value"] for d in dice_outcomes)
-    # anvil.alert(f"{results[0]['value']}")
-    self.label_2.text = results[0]["value"]
+    # anvil.alert(f"{results[0]}")
+    if results[0]["qty"] == 1 and results[0]["modifier"] == 0:
+      self.label_2.text = results[0]["value"]
+    else:
+      self.label_2.text = (
+        ", ".join(str(r["value"]) for r in results[0]["rolls"])
+          + (f"\n + {results[0]['modifier']}" if results[0]['modifier'] else "")
+          + "\n = " + str(results[0]["value"])
+      )
+      
     
     
